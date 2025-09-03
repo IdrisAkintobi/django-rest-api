ac87ae6f19f0622c35f340b97e1e2923c480cdf3
Idris Akintobi Adejare <akintobiidris@gmail.com>
2025-09-03 21:53:30 +0100

    fix: removed the use of username

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
-# Register your models here.
-class UserAdmin(UserAdmin):
-    list_display = ("username", "email", "first_name", "last_name")
+
+class UserAdmin(BaseUserAdmin):
+    # Show these in the user list
+    list_display = ("email", "is_staff", "is_superuser", "is_active")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    # Fields when editing a user
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("bio", "facebook", "linkedin", "profile_picture")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    # Fields when creating a new user
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = ()  # 🚨 disable groups/permissions
+
 
 admin.site.register(User, UserAdmin)
--- authapp/models.py
+++ authapp/models.py
@@ -1,14 +1,44 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
-class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)  # lowercases domain
+        user = self.model(email=email, **extra_fields)  # create user instance
+        user.set_password(password)  # hash password
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     bio = models.TextField(blank=True, default='')
-    profile_picture = models.ImageField(upload_to='profile_images', null=True)
+    email = models.EmailField(unique=True)
     facebook = models.URLField(null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     linkedin = models.URLField(null=True)
+    profile_picture = models.ImageField(upload_to='profile_images', null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
